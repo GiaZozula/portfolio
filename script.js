fdbdd30bf8ffd1b404414ce8ccb6d4275847163d
olstinker <giazozula@gmail.com>
2023-06-15 10:05:16 -0400

    minor fixes

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+// fade body in
+document.body.className = "visibleBod";
+
+// Scrolling
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('visible');
-        } else {
-            entry.target.classList.remove('visible'); 
-        }
-    });
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    } else {
+      entry.target.classList.remove("visible");
+    }
+  });
 });
 
-const hiddenElements = document.querySelectorAll('.hidden');
+const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((element) => observer.observe(element));
